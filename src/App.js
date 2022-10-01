11963c3b6fe37a943b4283fc8ec5366574b95971
ArshM17 <arshmaknojia5@gmail.com>
2022-10-01 16:44:53 +0530

    add header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import React , { useState } from 'react';
+import './App.css';
+import Dropdown from './Dropdown';
+
 function App() {
-  return;
+  return(
+    <>
+      <div className="header">
+        <div>
+          <div className="filter-container">
+            <span>Genre:</span>
+            <Dropdown/>
+          </div>
+          <div className="filter-container">
+            <span>Sort By:</span>
+            <Dropdown/>
+          </div>
+        </div>
+        <input type="text" placeholder="Search" className="search"></input>
+      </div>
+    </>
+  )
 }
 
 export default App;
